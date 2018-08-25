8c0e4e88bdc8a2a60426d8db1c01b1dabf22884f
Rupesh Chaudhari <rcashok0@gmail.com>
2018-08-26 02:32:28 +0530

    Updating Routes

--- api.py
+++ api.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
+import uuid
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 app = Flask(__name__)
 
@@ -16,6 +19,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     public_id = db.Column(db.String(50), unique=True)
     name = db.Column(db.String(80))
+    password = db.Column(db.String(50))
     admin = db.Column(db.Boolean)
 
 
@@ -28,17 +32,49 @@ class Todo(db.Model):
 
 @app.route('/user', methods=['GET'])
 def get_users():
-    return "<h1> Get All Users </h1> "
+    users = User.query.all()
+    ob = []
+    for user in users:
+        user_data = {}
+        user_data['id'] = user.id
+        user_data['public_id'] = user.public_id
+        user_data['name'] = user.name
+        user_data['password'] = user.password
+        user_data['admin'] = user.admin
+        ob.append(user_data)
+
+    return jsonify({"users": ob})
 
 
-@app.route('/user/<user_id>', methods=['GET'])
-def get_user(user_id):
-    return "<h1>Get  %s User</h1>" % user_id
+@app.route('/user/<public_id>', methods=['GET'])
+def get_user(public_id):
+    user = User.query.filter_by(public_id=public_id).first()
+
+    if not user:
+        return jsonify({"message": "No user Found!"})
+
+    user_data = {}
+    user_data['id'] = user.id
+    user_data['public_id'] = user.public_id
+    user_data['name'] = user.name
+    user_data['password'] = user.password
+    user_data['admin'] = user.admin
+
+    return jsonify({"user": user_data})
 
 
 @app.route('/user', methods=["POST"])
 def create_user():
-    return "<h1>Create User</h1>"
+    data = request.get_json()
+
+    hashed_pass = generate_password_hash(data["password"], method='sha256')
+
+    new_User = User(public_id=str(uuid.uuid4()),
+                    name=data["name"], password=hashed_pass, admin=False)
+    db.session.add(new_User)
+    db.session.commit()
+
+    return jsonify({'message': 'User Created Successfully....'})
 
 
 @app.route('/user/<user_id>', methods=["PUT"])
